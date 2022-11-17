ccedf64f065338bdf5bca67cf95741ae13ef4f61
Chris Chandler <christophergchandler@gmail.com>
2022-11-16 22:28:10 -0500

    Basic UI work - still needs number formatting and field constraints

--- ContractorCalculator/ViewController.swift
+++ ContractorCalculator/ViewController.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtLabor: UITextField!
+    @IBOutlet weak var txtMaterials: UITextField!
+    @IBOutlet weak var lblSubTotal: UILabel!
+    @IBOutlet weak var lblTax: UILabel!
+    @IBOutlet weak var lblTotal: UILabel!
+    
+    @IBAction func btnCalculate(_ sender: Any) {
+        
+        let labor = Double(txtLabor.text!)
+        let materials = Double(txtMaterials.text!)
+        let subTotal = labor! + materials!
+        let taxRate = 0.05
+        let tax = subTotal * taxRate
+        let total = subTotal + tax
+        
+        lblSubTotal.text = "\(subTotal)"
+        lblTax.text = "\(tax)"
+        lblTotal.text = "\(total)"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
